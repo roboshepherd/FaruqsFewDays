dc892036fb5c2bc87b72cc5c5575f9ff2b5e1a9b
M Omar Faruque Sarker <writefaruq@gmail.com>
2010-10-02 13:31:33 +0100

    publications added

---
+++ blade_packs/system_tinyMCE.php
@@ -0,0 +1,82 @@
+<?php
+///////////////////////////////////////////////////////////
+// morgan integrated systems limited - razorCMS          //
+// Blade Pack                                            //
+// tinyMCE                                               //
+// GPLv3                                                 //
+// Version 1.0                                           //
+// smiffy6969                                            //
+// www.razorcms.co.uk                                    //
+// www.mis-limited.com                                   //
+// 06/2009                                               //
+///////////////////////////////////////////////////////////
+
+///////////////////////
+// Socket Allocation //
+///////////////////////
+
+// Add Blades to Sockets $bladeList['blade'] = 'socket'; //
+$bladeList['adminHeadTinyMCE'] = 'admin-xhtml-head';
+$bladeList['tinyMCE'] = 'editor';
+
+///////////////////////
+//      Blades       //
+///////////////////////
+
+// blade - add script link to admin xhtml head //
+function adminHeadTinyMCE() {
+    if($_SESSION['adminLogIn']) {
+        echo '<script type="text/javascript" src="../blade_packs/system_tinyMCE/jscripts/tiny_mce/tiny_mce.js"></script>';
+    }
+}
+
+// blade - start editor //
+function tinyMCE(&$te) {
+    if($_SESSION['adminLogIn']) {
+        $editorCSSPath = "../";
+        $editorCSS = "theme/editor_css.css";
+        BsocketB( 'editor-css-path' , array( &$editorCSS ) );
+
+?>
+        <!-- TinyMCE -->
+        <script type="text/javascript">
+	tinyMCE.init({
+		// General options
+		mode : "textareas",
+		theme : "advanced",
+		plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+
+		// Theme options
+		theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
+		theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+		theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+		theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
+		theme_advanced_toolbar_location : "top",
+		theme_advanced_toolbar_align : "left",
+		theme_advanced_statusbar_location : "bottom",
+		theme_advanced_resizing : true,
+
+		// Example content CSS (should be your site CSS)
+		content_css : "<?php echo $editorCSSPath.$editorCSS; ?>",
+
+		// Drop lists for link/image/media/template dialogs
+		template_external_list_url : "lists/template_list.js",
+		external_link_list_url : "lists/link_list.js",
+		external_image_list_url : "lists/image_list.js",
+		media_external_list_url : "lists/media_list.js",
+
+		// Replace values for the template plugin
+		template_replace_values : {
+			username : "Some User",
+			staffid : "991234"
+		}
+	});
+        </script>
+        <!-- /TinyMCE -->
+<?php
+	$te[2] = '<textarea id="elm1" name="content" rows="30" style="width: 98%">'.$te[4].'</textarea>';
+    }
+}
+// end ///////////////////
+
+?>
